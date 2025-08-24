35377b1a25ffcc515f26b75a2906b10b53965801
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-24 23:52:09 +0530

    First commit

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <windows.h>
+#include <sql.h>
+#include <sqlext.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+
+SQLHENV env = NULL;
+SQLHDBC dbc = NULL;
+string currentUserRole;
+
+
+void showError(const char* fn, SQLHANDLE handle, SQLSMALLINT type) {
+    SQLINTEGER i = 0, native;
+    SQLCHAR state[7], text[256];
+    SQLSMALLINT len;
+    while (SQLGetDiagRec(type, handle, ++i, state, &native, text, sizeof(text), &len) == SQL_SUCCESS)
+        cerr << fn << " error: " << state << " " << native << " " << text << "\n";
+}
+
+
+bool exists(const string& sql) {
+    SQLHSTMT stmt;
+    if (!SQL_SUCCEEDED(SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt))) return false;
+    if (!SQL_SUCCEEDED(SQLExecDirect(stmt, (SQLCHAR*)sql.c_str(), SQL_NTS))) {
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt); return false;
+    }
+    SQLRETURN r = SQLFetch(stmt);
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+    return (r == SQL_SUCCESS || r == SQL_SUCCESS_WITH_INFO);
+}
+
+
+bool execSQL(const string& sql) {
+    SQLHSTMT stmt;
+    if (!SQL_SUCCEEDED(SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt))) return false;
+    SQLRETURN ret = SQLExecDirect(stmt, (SQLCHAR*)sql.c_str(), SQL_NTS);
+    if (!SQL_SUCCEEDED(ret)) showError("execSQL", stmt, SQL_HANDLE_STMT);
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+    return SQL_SUCCEEDED(ret);
+}
+
+bool login() {
+    string username, password;
+    cout << "\n=== Login ===\nUsername: ";
+    getline(cin, username);
+    cout << "Password: ";
+    getline(cin, password);  
+
+    string sql = "SELECT role, status FROM users WHERE username='" + username +
+                 "' AND passwordhash='" + password + "'";
+    
+    SQLHSTMT stmt;
+    if (!SQL_SUCCEEDED(SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt))) return false;
+    if (!SQL_SUCCEEDED(SQLExecDirect(stmt, (SQLCHAR*)sql.c_str(), SQL_NTS))) {
+        showError("Login", stmt, SQL_HANDLE_STMT);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt); return false;
+    }
+
+    SQLCHAR role[20], status[20];
+    if (SQLFetch(stmt) == SQL_SUCCESS) {
+        SQLGetData(stmt, 1, SQL_C_CHAR, role, sizeof(role), NULL);
+        SQLGetData(stmt, 2, SQL_C_CHAR, status, sizeof(status), NULL);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+
+        if (string((char*)status) == "Inactive") {
+            cout << "Account is inactive.\n";
+            return false;
+        }
+
+        currentUserRole = (char*)role;
+        cout << "Login successful. Role: " << currentUserRole << "\n";
+        return true;
+    } else {
+        cout << "Invalid credentials.\n";
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+        return false;
+    }
+}
